a83f809b6110f12cefd110521bd193538c34bfca
yuji91 <yuji91.1227@gmail.com>
2019-04-16 22:02:08 +0900

    Refactor

--- lib/item_stock.rb
+++ lib/item_stock.rb
@@ -4,36 +4,36 @@ class ItemStock
   QUANTITY_OF_TEA = 5 # お茶の在庫数
 
   def initialize(kindOfDrink)
-    @kindOfDrink = kindOfDrink
+    @kind_of_drink = kindOfDrink
     @quantity_of_item = if kindOfDrink == Drink::COKE
-                        QUANTITY_OF_COKE
-                      elsif kindOfDrink == Drink::DIET_COKE
-                        QUANTITY_OF_DIET_COKE
-                      elsif kindOfDrink == Drink::TEA
-                        QUANTITY_OF_TEA
-                      end
+                          QUANTITY_OF_COKE
+                        elsif kindOfDrink == Drink::DIET_COKE
+                          QUANTITY_OF_DIET_COKE
+                        elsif kindOfDrink == Drink::TEA
+                          QUANTITY_OF_TEA
+                        end
   end
-  
-  def kindOfDrink
-    @kindOfDrink
+
+  def kind_of_drink
+    @kind_of_drink
   end
 
   def empty?
-    if kindOfDrink == Drink::COKE
+    if kind_of_drink == Drink::COKE
       QUANTITY_OF_COKE.zero?
-    elsif kindOfDrink == Drink::COKE
+    elsif kind_of_drink == Drink::COKE
       QUANTITY_OF_DIET_COKE.zero?
-    elsif kindOfDrink == Drink::TEA
+    elsif kind_of_drink == Drink::TEA
       QUANTITY_OF_TEA.zero?
     end
   end
 
   def calculate
-    if kindOfDrink == Drink::COKE
+    if kind_of_drink == Drink::COKE
       @quantity_of_item -= 1
-    elsif kindOfDrink == Drink::DIET_COKE
+    elsif kind_of_drink == Drink::DIET_COKE
       @quantity_of_item -= 1
-    elsif kindOfDrink == Drink::TEA
+    elsif kind_of_drink == Drink::TEA
       @quantity_of_item -= 1
     end
   end
